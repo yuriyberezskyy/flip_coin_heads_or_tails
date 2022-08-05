04125421546cc0ef0b1ba14cbe4c34b0d760ee6f
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-05 00:26:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+flip_coin = random.randint(0,1)
+
+if flip_coin == 0:
+  print("Tails")
+
+if flip_coin == 1:
+  print("Heads")
+
+
+
+
+
+
+
+
+
+
